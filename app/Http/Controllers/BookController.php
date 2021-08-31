c519a3b28b7d5832b2aca362eb62f2c534cfcc21
lani <lani.requilman>
2021-08-31 19:06:18 +0800

    laravel vue SPA test

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //all books
+    public function index()
+    {
+        $books = Book::all()->toArray();
+        return array_reverse($books);
+    }
+    //add books
+    public function add(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:255',
+            'author' => 'required'
+        ]);
+        $book = new Book([
+            'name' => $request->input('name'),
+            'author' => $request->input('author')
+        ]);
+        $book->save();
+
+        return response()->json('The book successfully added');
+    }
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Book $book)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    //edit book
+    public function edit($id)
+    {
+        $book = Book::find($id);
+        return response()->json($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    //update book
+    public function update($id, Request $request)
+    {
+        $book = Book::find($id);
+        $book->update($request->all());
+
+        return response()->json('The book successfully updated');
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Book  $book
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Book $book)
+    {
+        //
+    }
+    //delete book
+    public function delete($id)
+    {
+        $book = Book::find($id);
+        $book->delete();
+
+        return response()->json('The book successfully deleted');
+    }
+}
---
+++ resources/js/pages/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="row">
+    <div class="col-lg-10 m-auto">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <router-link to="books" class="nav-item nav-link">All Books</router-link>
+                    <router-link to="/addbooks" class="nav-item nav-link">Add Book</router-link>
+                </div>
+            </div>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+// import axios from 'axios'
+export default {
+  middleware: 'auth',
+
+  // async asyncData () {
+  //   const { data: projects } = await axios.get('/api/projects')
+
+  //   return {
+  //     projects
+  //   }
+  // },
+
+  metaInfo () {
+    return { title: this.$t('home') }
+  }
+}
+</script>
